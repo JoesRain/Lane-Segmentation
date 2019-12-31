1caca33c30557d7109c40af078ae8cdd75315ed8
颜忠贤 <yanzx001@cmft.com>
2019-12-31 17:49:13 +0800

    替换三个模型

--- models/deeplabv3p.py
+++ models/deeplabv3p.py
@@ -1,344 +1,206 @@
-# Deeplabv3p Network is modified from the following link
-# https://github.com/PaddlePaddle/models/blob/develop/fluid/PaddleCV/deeplabv3%2B/models.py
+import torch.nn as nn
+import torch
+import numpy as np
+affine_par = True
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-import paddle.fluid as fluid
+def outS(i):
+    i = int(i)
+    i = (i+1)/2
+    i = int(np.ceil((i+1)/2.0))
+    i = (i+1)/2
+    return i
 
-import contextlib
-name_scope = ""
-
-decode_channel = 32
-encode_channel = 160
-
-bn_momentum = 0.99
-
-op_results = {}
-
-default_epsilon = 1e-3
-default_norm_type = 'bn'
-default_group_number = 32
-depthwise_use_cudnn = True
-
-bn_regularizer = fluid.regularizer.L2DecayRegularizer(regularization_coeff=0.0)
-depthwise_regularizer = fluid.regularizer.L2DecayRegularizer(
-    regularization_coeff=0.0)
+def conv3x3(in_planes, out_planes, stride=1):
+    "3x3 convolution with padding"
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
 
 
-@contextlib.contextmanager
-def scope(name):
-    global name_scope
-    bk = name_scope
-    name_scope = name_scope + name + '/'
-    yield
-    name_scope = bk
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes, affine = affine_par)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes, affine = affine_par)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
 
 
-def check(data, number):
-    if type(data) == int:
-        return [data] * number
-    assert len(data) == number
-    return data
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1,  dilation_ = 1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False) # change
+        self.bn1 = nn.BatchNorm2d(planes,affine = affine_par)
+    for i in self.bn1.parameters():
+            i.requires_grad = False
+        padding = 1
+        if dilation_ == 2:
+        padding = 2
+        elif dilation_ == 4:
+        padding = 4
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, # change
+                               padding=padding, bias=False, dilation = dilation_)
+        self.bn2 = nn.BatchNorm2d(planes,affine = affine_par)
+        for i in self.bn2.parameters():
+            i.requires_grad = False
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4, affine = affine_par)
+        for i in self.bn3.parameters():
+            i.requires_grad = False
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
 
 
-def clean():
-    global op_results
-    op_results = {}
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class Classifier_Module(nn.Module):
+
+    def __init__(self,dilation_series,padding_series,NoLabels):
+        super(Classifier_Module, self).__init__()
+	self.conv2d_list = nn.ModuleList()
+	for dilation,padding in zip(dilation_series,padding_series):
+	    self.conv2d_list.append(nn.Conv2d(2048,NoLabels,kernel_size=3,stride=1, padding =padding, dilation = dilation,bias = True))
+
+        for m in self.conv2d_list:
+            m.weight.data.normal_(0, 0.01)
 
 
-def append_op_result(result, name):
-    global op_results
-    op_index = len(op_results)
-    name = name_scope + name + str(op_index)
-    op_results[name] = result
-    return result
+    def forward(self, x):
+	out = self.conv2d_list[0](x)
+	for i in range(len(self.conv2d_list)-1):
+	    out += self.conv2d_list[i+1](x)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, block, layers,NoLabels):
+        self.inplanes = 64
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64,affine = affine_par)
+        for i in self.bn1.parameters():
+            i.requires_grad = False
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=True) # change
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=1, dilation__ = 2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=1, dilation__ = 4)
+	self.layer5 = self._make_pred_layer(Classifier_Module, [6,12,18,24],[6,12,18,24],NoLabels)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, 0.01)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+        #        for i in m.parameters():
+        #            i.requires_grad = False
+
+    def _make_layer(self, block, planes, blocks, stride=1,dilation__ = 1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion or dilation__ == 2 or dilation__ == 4:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion,affine = affine_par),
+            )
+        for i in downsample._modules['1'].parameters():
+            i.requires_grad = False
+        layers = []
+        layers.append(block(self.inplanes, planes, stride,dilation_=dilation__, downsample = downsample ))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes,dilation_=dilation__))
+
+        return nn.Sequential(*layers)
+    def _make_pred_layer(self,block, dilation_series, padding_series,NoLabels):
+	return block(dilation_series,padding_series,NoLabels)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+	x = self.layer5(x)
+
+        return x
+
+class MS_Deeplab(nn.Module):
+    def __init__(self,block,NoLabels):
+	super(MS_Deeplab,self).__init__()
+	self.Scale = ResNet(block,[3, 4, 23, 3],NoLabels)   #changed to fix #4
+
+    def forward(self,x):
+        input_size = x.size()[2]
+	    self.interp1 = nn.UpsamplingBilinear2d(size = (  int(input_size*0.75)+1,  int(input_size*0.75)+1  ))
+        self.interp2 = nn.UpsamplingBilinear2d(size = (  int(input_size*0.5)+1,   int(input_size*0.5)+1   ))
+        self.interp3 = nn.UpsamplingBilinear2d(size = (  outS(input_size),   outS(input_size)   ))
+        out = []
+        x2 = self.interp1(x)
+        x3 = self.interp2(x)
+	    out.append(self.Scale(x))	# for original scale
+	    out.append(self.interp3(self.Scale(x2)))	# for 0.75x scale
+	    out.append(self.Scale(x3))	# for 0.5x scale
 
 
-def conv(*args, **kargs):
-    if "xception" in name_scope:
-        init_std = 0.09
-    elif "logit" in name_scope:
-        init_std = 0.01
-    elif name_scope.endswith('depthwise/'):
-        init_std = 0.33
-    else:
-        init_std = 0.06
-    if name_scope.endswith('depthwise/'):
-        regularizer = depthwise_regularizer
-    else:
-        regularizer = None
+        x2Out_interp = out[1]
+        x3Out_interp = self.interp3(out[2])
+        temp1 = torch.max(out[0],x2Out_interp)
+        out.append(torch.max(temp1,x3Out_interp))
+	    return out
 
-    kargs['param_attr'] = fluid.ParamAttr(
-        name=name_scope + 'weights',
-        regularizer=regularizer,
-        initializer=fluid.initializer.TruncatedNormal(
-            loc=0.0, scale=init_std))
-    if 'bias_attr' in kargs and kargs['bias_attr']:
-        kargs['bias_attr'] = fluid.ParamAttr(
-            name=name_scope + 'biases',
-            regularizer=regularizer,
-            initializer=fluid.initializer.ConstantInitializer(value=0.0))
-    else:
-        kargs['bias_attr'] = False
-    kargs['name'] = name_scope + 'conv'
-    return append_op_result(fluid.layers.conv2d(*args, **kargs), 'conv')
-
-
-def group_norm(input, G, eps=1e-5, param_attr=None, bias_attr=None):
-    N, C, H, W = input.shape
-    if C % G != 0:
-        # print "group can not divide channle:", C, G
-        for d in range(10):
-            for t in [d, -d]:
-                if G + t <= 0: continue
-                if C % (G + t) == 0:
-                    G = G + t
-                    break
-            if C % G == 0:
-                # print "use group size:", G
-                break
-    assert C % G == 0
-    x = fluid.layers.group_norm(
-        input,
-        groups=G,
-        param_attr=param_attr,
-        bias_attr=bias_attr,
-        name=name_scope + 'group_norm')
-    return x
-
-
-def bn(*args, **kargs):
-    if default_norm_type == 'bn':
-        with scope('BatchNorm'):
-            return append_op_result(
-                fluid.layers.batch_norm(
-                    *args,
-                    epsilon=default_epsilon,
-                    momentum=bn_momentum,
-                    param_attr=fluid.ParamAttr(
-                        name=name_scope + 'gamma', regularizer=bn_regularizer),
-                    bias_attr=fluid.ParamAttr(
-                        name=name_scope + 'beta', regularizer=bn_regularizer),
-                    moving_mean_name=name_scope + 'moving_mean',
-                    moving_variance_name=name_scope + 'moving_variance',
-                    **kargs),
-                'bn')
-    elif default_norm_type == 'gn':
-        with scope('GroupNorm'):
-            return append_op_result(
-                group_norm(
-                    args[0],
-                    default_group_number,
-                    eps=default_epsilon,
-                    param_attr=fluid.ParamAttr(
-                        name=name_scope + 'gamma', regularizer=bn_regularizer),
-                    bias_attr=fluid.ParamAttr(
-                        name=name_scope + 'beta', regularizer=bn_regularizer)),
-                'gn')
-    else:
-        raise "Unsupport norm type:" + default_norm_type
-
-
-def bn_relu(data):
-    return append_op_result(fluid.layers.relu(bn(data)), 'relu')
-
-
-def relu(data):
-    return append_op_result(
-        fluid.layers.relu(
-            data, name=name_scope + 'relu'), 'relu')
-
-
-def seperate_conv(input, channel, stride, filter, dilation=1, act=None):
-    with scope('depthwise'):
-        input = conv(
-            input,
-            input.shape[1],
-            filter,
-            stride,
-            groups=input.shape[1],
-            padding=(filter // 2) * dilation,
-            dilation=dilation,
-            use_cudnn=depthwise_use_cudnn)
-        input = bn(input)
-        if act: input = act(input)
-    with scope('pointwise'):
-        input = conv(input, channel, 1, 1, groups=1, padding=0)
-        input = bn(input)
-        if act: input = act(input)
-    return input
-
-
-def xception_block(input,
-                   channels,
-                   strides=1,
-                   filters=3,
-                   dilation=1,
-                   skip_conv=True,
-                   has_skip=True,
-                   activation_fn_in_separable_conv=False):
-    repeat_number = 3
-    channels = check(channels, repeat_number)
-    filters = check(filters, repeat_number)
-    strides = check(strides, repeat_number)
-    data = input
-    results = []
-    for i in range(repeat_number):
-        with scope('separable_conv' + str(i + 1)):
-            if not activation_fn_in_separable_conv:
-                data = relu(data)
-                data = seperate_conv(
-                    data,
-                    channels[i],
-                    strides[i],
-                    filters[i],
-                    dilation=dilation)
-            else:
-                data = seperate_conv(
-                    data,
-                    channels[i],
-                    strides[i],
-                    filters[i],
-                    dilation=dilation,
-                    act=relu)
-            results.append(data)
-    if not has_skip:
-        return append_op_result(data, 'xception_block'), results
-    if skip_conv:
-        with scope('shortcut'):
-            skip = bn(
-                conv(
-                    input, channels[-1], 1, strides[-1], groups=1, padding=0))
-    else:
-        skip = input
-    return append_op_result(data + skip, 'xception_block'), results
-
-
-def entry_flow(data):
-    with scope("entry_flow"):
-        with scope("conv1"):
-            data = conv(data, 32, 3, stride=2, padding=1)
-            data = bn_relu(data)
-        with scope("conv2"):
-            data = conv(data, 32, 3, stride=1, padding=1)
-            data = bn_relu(data)
-        with scope("block1"):
-            data, results1 = xception_block(data, 64, [1, 1, 2])
-        with scope("block2"):
-            data, results2 = xception_block(data, 128, [1, 1, 2])
-        with scope("block3"):
-            data, _ = xception_block(data, 256, [1, 1, 2])
-        return data, results1[1], results2[2]
-
-
-def middle_flow(data):
-    with scope("middle_flow"):
-        for i in range(8):
-            with scope("block" + str(i + 1)):
-                data, _ = xception_block(data, 256, [1, 1, 1], skip_conv=False)
-    return data
-
-
-def exit_flow(data):
-    with scope("exit_flow"):
-        with scope('block1'):
-            data, _ = xception_block(data, [256, 512, 512], [1, 1, 1])
-        with scope('block2'):
-            data, _ = xception_block(
-                data, [512, 512, 768], [1, 1, 1],
-                dilation=2,
-                has_skip=False,
-                activation_fn_in_separable_conv=True)
-        return data
-
-
-def encoder(input):
-    with scope('encoder'):
-        channel = 192
-
-        with scope("aspp0"):
-            aspp0 = bn_relu(conv(input, channel, 1, 1, groups=1, padding=0))
-        with scope("aspp1"):
-            aspp1 = seperate_conv(input, channel, 1, 3, dilation=3, act=relu)
-        with scope("aspp2"):
-            aspp2 = seperate_conv(input, channel, 1, 3, dilation=6, act=relu)
-        with scope("aspp3"):
-            aspp3 = seperate_conv(input, channel, 1, 3, dilation=12, act=relu)
-        with scope("concat"):
-            data = append_op_result(
-                fluid.layers.concat(
-                    [aspp0, aspp1, aspp2, aspp3], axis=1),
-                'concat')
-            data = bn_relu(conv(data, channel, 1, 1, groups=1, padding=0))
-        return data
-
-
-def decoder(encode_data, decode_shortcut):
-    with scope('decoder'):
-        with scope('concat'):
-            decode_shortcut = bn_relu(
-                conv(
-                    decode_shortcut, decode_channel, 1, 1, groups=1, padding=0))
-            encode_data = fluid.layers.resize_bilinear(
-                encode_data, decode_shortcut.shape[2:])
-            encode_data = fluid.layers.concat(
-                [encode_data, decode_shortcut], axis=1)
-            append_op_result(encode_data, 'concat')
-        with scope("separable_conv1"):
-            encode_data = seperate_conv(
-                encode_data, encode_channel, 1, 3, dilation=1, act=relu)
-        with scope("separable_conv2"):
-            encode_data = seperate_conv(
-                encode_data, encode_channel, 1, 3, dilation=1, act=relu)
-        return encode_data
-
-def decoder2(encode_data, decode_shortcut):
-    with scope('decoder2'):
-        with scope('concat2'):
-            decode_shortcut = bn_relu(
-                conv(
-                    decode_shortcut, decode_channel // 2, 1, 1, groups=1, padding=0))
-            encode_data = fluid.layers.resize_bilinear(
-                encode_data, decode_shortcut.shape[2:])
-            encode_data = fluid.layers.concat(
-                [encode_data, decode_shortcut], axis=1)
-            append_op_result(encode_data, 'concat2')
-        with scope("separable_conv12"):
-            encode_data = seperate_conv(
-                encode_data, encode_channel // 2, 1, 3, dilation=1, act=relu)
-        with scope("separable_conv22"):
-            encode_data = seperate_conv(
-                encode_data, encode_channel // 2, 1, 3, dilation=1, act=relu)
-        return encode_data
-
-
-def deeplabv3p(img, label_number):
-    global default_epsilon
-    append_op_result(img, 'img')
-    with scope('xception_65'):
-        default_epsilon = 1e-3
-        # Entry flow
-        data, decode_shortcut1, decode_shortcut2 = entry_flow(img)
-        print(data.shape)
-        # Middle flow
-        data = middle_flow(data)
-        print(data.shape)
-        # Exit flow
-        data = exit_flow(data)
-        print(data.shape)
-    default_epsilon = 1e-5
-    encode_data = encoder(data)
-    print(encode_data.shape)
-    encode_data = decoder(encode_data, decode_shortcut2)
-    print(encode_data.shape)
-    encode_data = fluid.layers.resize_bilinear(encode_data, (encode_data.shape[2] * 2, encode_data.shape[3] * 2))
-    encode_data = decoder2(encode_data, decode_shortcut1)
-    print(encode_data.shape)
-    with scope('logit'):
-        logit = conv(
-            encode_data, label_number, 1, stride=1, padding=0, bias_attr=True)
-        logit = fluid.layers.resize_bilinear(logit, img.shape[2:])
-        # logit = fluid.layers.resize_bilinear(logit, (3384, 1020))
-        print(logit.shape)
-    return logit
+def Res_Deeplab(NoLabels=21):
+    model = MS_Deeplab(Bottleneck,NoLabels)
+    return model
--- models/unet_simple.py
+++ models/unet_simple.py
@@ -1,110 +1,78 @@
-# Author: Jingxiao Gu
-# Baidu Account: Seigato
-# Description: Unet Simple Network for Lane Segmentation Competition
+import torch
+import torch.nn as nn
 
-import paddle.fluid as fluid
-from paddle.fluid.initializer import MSRA
-from paddle.fluid.param_attr import ParamAttr
+class double_conv(nn.Module):
+    '''(conv => BN => ReLU) * 2'''
+    def __init__(self, in_ch, out_ch):
+        super(double_conv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
 
-def conv_bn_layer(input, num_filters, filter_size, stride=1, groups=1, act=None, bn=True, bias_attr=False):
-    conv = fluid.layers.conv2d(
-            input=input,
-            num_filters=num_filters,
-            filter_size=filter_size,
-            stride=stride,
-            padding=(filter_size - 1) // 2,
-            groups=groups,
-            act=None,
-            bias_attr=bias_attr,
-            param_attr=ParamAttr(initializer=MSRA()))
-    if bn == True:
-          conv = fluid.layers.batch_norm(input=conv, act=act)
-    return conv
+    def forward(self, x):
+        x = self.conv(x)
+        return x
 
-def conv_layer(input, num_filters, filter_size, stride=1, groups=1, act=None):
-    conv = fluid.layers.conv2d(
-            input=input,
-            num_filters=num_filters,
-            filter_size=filter_size,
-            stride=stride,
-            padding=(filter_size - 1) // 2,
-            groups=groups,
-            act=act,
-            bias_attr=ParamAttr(initializer=MSRA()),
-            param_attr=ParamAttr(initializer=MSRA()))
-    return conv
 
-def shortcut(input, ch_out, stride):
-    ch_in = input.shape[1]
-    if ch_in != ch_out or stride != 1:
-        return conv_bn_layer(input, ch_out, 1, stride)
-    else:
-        return input
+class up(nn.Module):
+    def __init__(self, in_ch, out_ch, bilinear=True):
+        super(up, self).__init__()
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_ch, out_ch, 2, stride=2)
+    def forward(self, x):
+        x = self.up(x)
+        return x
 
-def bottleneck_block(input, num_filters, stride):
-    conv_bn = conv_bn_layer(input=input, num_filters=num_filters, filter_size=3, act='relu')
-    conv_bn = conv_bn_layer(input=conv_bn, num_filters=num_filters, filter_size=3, stride=stride, act=None)
-    short_bn = shortcut(input, num_filters, stride)
-    return fluid.layers.elementwise_add(x=short_bn, y=conv_bn, act='relu')
 
-def encoder_block(input, encoder_depths, encoder_filters, block):
-    conv_bn = input
-    for i in range(encoder_depths[block]):
-        conv_bn = bottleneck_block(input=conv_bn, num_filters=encoder_filters[block], stride=2 if i == 0 and block != 0 else 1)
-    print("| Encoder Block", block, conv_bn.shape)
-    return conv_bn
+class MyUNet(nn.Module):
+    '''Mixture of previous classes'''
 
-def decoder_block(input, concat_input, decoder_depths, decoder_filters, block):
-    deconv_bn = input
-    deconv_bn = fluid.layers.resize_bilinear(input=deconv_bn, out_shape=(deconv_bn.shape[2] * 2, deconv_bn.shape[3] * 2))
-    deconv_bn = bottleneck_block(input=deconv_bn, num_filters=decoder_filters[block], stride=1)
+    def __init__(self,in_ch, out_ch, n_classes):
+        super(MyUNet, self).__init__()
+        self.conv1 = double_conv(in_ch, 64)
+        self.conv2 = double_conv(64, 128)
+        self.conv3 = double_conv(128, 256)
+        self.conv4 = double_conv(256, 512)
+        self.conv5 = double_conv(512, 1024)
+        self.conv6 = double_conv(1024, 512)
+        self.conv7 = double_conv(512, 256)
+        self.conv8 = double_conv(256, 128)
+        self.conv9 = double_conv(128, 64)
+        self.conv10 = double_conv(64, out_ch,1)
 
-    concat_input = conv_bn_layer(input=concat_input, num_filters=concat_input.shape[1] // 2, filter_size=1, act='relu')
+        self.mp = nn.MaxPool2d(2)
+        self.up1 = up(1024, 512)
+        self.up2 = up(512, 256)
+        self.up3 = up(256, 128)
+        self.up4 = up(128, 64)
+    def forward(self, x):
+        x1 = self.mp(self.conv0(x))
+        x2 = self.mp(self.conv1(x1))
+        x3 = self.mp(self.conv2(x2))
+        x4 = self.mp(self.conv3(x3))
+        x5 = self.mp(self.conv4(x4))
 
-    deconv_bn = fluid.layers.concat([deconv_bn, concat_input], axis=1)
-    for i in range(decoder_depths[block]):
-        deconv_bn = bottleneck_block(input=deconv_bn, num_filters=decoder_filters[block], stride=1)
-    print('| Decoder Block', block, deconv_bn.shape)
-    return deconv_bn
+        up_1 = self.up1(x5)
+        merge1 = torch.cat([up_1, x4], dim=1)
+        x6 = self.conv6(merge1)
+        up_2 = self.up2(x6)
 
-def unet_simple(img, label_number, img_size):
-    print("| Build Custom-Designed Resnet-Unet:")
-    encoder_depth = [3, 4, 5, 3]
-    encoder_filters = [64, 128, 256, 512]
-    decoder_depth = [2, 3, 3, 2]
-    decoder_filters = [256, 128, 64, 32]
-    print('| Input Image Data', img.shape)
-    """ 
-    Encoder
-    """
-    # Start Conv
-    start_conv = conv_bn_layer(input=img, num_filters=32, filter_size=3, stride=2, act='relu')
-    start_conv = conv_bn_layer(input=start_conv, num_filters=32, filter_size=3, stride=1, act='relu')
-    start_pool = fluid.layers.pool2d(input=start_conv, pool_size=3, pool_stride=2, pool_padding=1, pool_type='max')
-    print('| Start Convolution', start_conv.shape)
+        merge2 = torch.cat([up_2, x3], dim=1)
+        x7 = self.conv7(merge2)
+        up_3 = self.up3(x7)
 
-    conv0 = encoder_block(start_pool, encoder_depth, encoder_filters, block=0)
-    conv1 = encoder_block(conv0, encoder_depth, encoder_filters, block=1)
-    conv2 = encoder_block(conv1, encoder_depth, encoder_filters, block=2)
-    conv3 = encoder_block(conv2, encoder_depth, encoder_filters, block=3)
+        merge3 = torch.cat([up_3, x2], dim=1)
+        x8 = self.conv8(merge3)
+        up_4 = self.up4(x8)
 
-    """ 
-    Decoder
-    """
-    decode_conv1 = decoder_block(conv3, conv2, decoder_depth, decoder_filters, block=0)
-    decode_conv2 = decoder_block(decode_conv1, conv1, decoder_depth, decoder_filters, block=1)
-    decode_conv3 = decoder_block(decode_conv2, conv0, decoder_depth, decoder_filters, block=2)
-    decode_conv4 = decoder_block(decode_conv3, start_conv, decoder_depth, decoder_filters, block=3)
-
-    """ 
-    Output Coder
-    """
-    decode_conv5 = fluid.layers.resize_bilinear(input=decode_conv4, out_shape=img_size)
-    decode_conv5 = bottleneck_block(input=decode_conv5, num_filters=32, stride=1)
-    decode_conv5 = bottleneck_block(input=decode_conv5, num_filters=16, stride=1)
-    logit = conv_layer(input=decode_conv5, num_filters=label_number, filter_size=1, act=None)
-    print("| Output Predictions:", logit.shape)
-    # logit = fluid.layers.resize_bilinear(input=logit, out_shape=(3384, 1020))
-    print("| Final Predictions:", logit.shape)
-
-    return logit
+        merge4 = torch.cat([up_4, x1], dim=1)
+        x9 = self.conv9(merge4)
+        x10 = self.conv10(x9)
+        return x10
