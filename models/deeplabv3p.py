325f9a74602965d819f9c53faddbc96b7f579c2a
颜忠贤 <yanzx001@cmft.com>
2020-01-05 11:52:47 +0800

    修复BUG

--- models/deeplabv3p.py
+++ models/deeplabv3p.py
@@ -165,12 +165,12 @@ class ResNet(nn.Module):
 
 class MS_Deeplab(nn.Module):
     def __init__(self,block,NoLabels):
-    super(MS_Deeplab,self).__init__()
-    self.Scale = ResNet(block,[3, 4, 23, 3],NoLabels)   #changed to fix #4
+        super(MS_Deeplab,self).__init__()
+        self.Scale = ResNet(block,[3, 4, 23, 3],NoLabels)   #changed to fix #4
 
     def forward(self,x):
         input_size = x.size()[2]
-    self.interp1 = nn.UpsamplingBilinear2d(size = (  int(input_size*0.75)+1,  int(input_size*0.75)+1  ))
+        self.interp1 = nn.UpsamplingBilinear2d(size = (  int(input_size*0.75)+1,  int(input_size*0.75)+1  ))
         self.interp2 = nn.UpsamplingBilinear2d(size = (  int(input_size*0.5)+1,   int(input_size*0.5)+1   ))
         self.interp3 = nn.UpsamplingBilinear2d(size = (  outS(input_size),   outS(input_size)   ))
         out = []
