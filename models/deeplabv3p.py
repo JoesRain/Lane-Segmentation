f894169c3e19efaa21fb2fbb3fc8da6930d00744
颜忠贤 <yanzx001@cmft.com>
2020-01-05 11:27:49 +0800

    修复BUG

--- models/deeplabv3p.py
+++ models/deeplabv3p.py
@@ -98,14 +98,13 @@ class Classifier_Module(nn.Module):
         self.conv2d_list = nn.ModuleList()
         for dilation,padding in zip(dilation_series,padding_series):
             self.conv2d_list.append(nn.Conv2d(2048,NoLabels,kernel_size=3,stride=1, padding =padding, dilation = dilation,bias = True))
-
-            for m in self.conv2d_list:
-                m.weight.data.normal_(0, 0.01)
+        for m in self.conv2d_list:
+            m.weight.data.normal_(0, 0.01)
 
     def forward(self, x):
         out = self.conv2d_list[0](x)
         for i in range(len(self.conv2d_list)-1):
-        out += self.conv2d_list[i+1](x)
+            out += self.conv2d_list[i+1](x)
         return out
 
 class ResNet(nn.Module):
