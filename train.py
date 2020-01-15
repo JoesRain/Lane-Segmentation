223b48a4de840958d07609be29df2768722cb700
yanzx <505554859@qq.com>
2020-01-15 21:27:43 +0800

    Default Changelist

--- train.py
+++ train.py
@@ -10,14 +10,13 @@ from utils.image_process import LaneDataset, ImageAug, DeformAug
 from utils.image_process import ScaleAug, CutOut, ToTensor
 from utils.loss import MySoftmaxCrossEntropyLoss
 from model.deeplabv3plus import DeeplabV3Plus
-from model.unet import UNet
 from config import Config
 
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = "7"
 
-device_list = [2,3]
-flag = True
+device_list = [0]
+
 
 def train_epoch(net, epoch, dataLoader, optimizer, trainF, config):
     net.train()
@@ -25,7 +24,7 @@ def train_epoch(net, epoch, dataLoader, optimizer, trainF, config):
     dataprocess = tqdm(dataLoader)
     for batch_item in dataprocess:
         image, mask = batch_item['image'], batch_item['mask']
-        if flag:
+        if torch.cuda.is_available():
             image, mask = image.cuda(device=device_list[0]), mask.cuda(device=device_list[0])
         optimizer.zero_grad()
         out = net(image)
@@ -39,17 +38,17 @@ def train_epoch(net, epoch, dataLoader, optimizer, trainF, config):
     trainF.flush()
 
 
-def val_epoch(net, epoch, dataLoader, testF, config):
+def test(net, epoch, dataLoader, testF, config):
     net.eval()
     total_mask_loss = 0.0
     dataprocess = tqdm(dataLoader)
     result = {"TP": {i:0 for i in range(8)}, "TA":{i:0 for i in range(8)}}
     for batch_item in dataprocess:
         image, mask = batch_item['image'], batch_item['mask']
-        if flag:
+        if torch.cuda.is_available():
             image, mask = image.cuda(device=device_list[0]), mask.cuda(device=device_list[0])
         out = net(image)
-        mask_loss = MySoftmaxCrossEntropyLoss(nbclasses=config.NUM_CLASSES)(out[0], mask)
+        mask_loss = MySoftmaxCrossEntropyLoss(nbclasses=config.NUM_CLASSES)(out, mask)
         total_mask_loss += mask_loss.detach().item()
         pred = torch.argmax(F.softmax(out, dim=1), dim=1)
         result = compute_iou(pred, mask, result)
@@ -86,7 +85,7 @@ def main():
     os.makedirs(lane_config.SAVE_PATH, exist_ok=True)
     trainF = open(os.path.join(lane_config.SAVE_PATH, "train.csv"), 'w')
     testF = open(os.path.join(lane_config.SAVE_PATH, "test.csv"), 'w')
-    kwargs = {'num_workers': 4, 'pin_memory': True} if flag else {}
+    kwargs = {'num_workers': 4, 'pin_memory': True} if torch.cuda.is_available() else {}
     train_dataset = LaneDataset("train.csv", transform=transforms.Compose([ImageAug(), DeformAug(),
                                                                               ScaleAug(), CutOut(32, 0.5), ToTensor()]))
 
@@ -95,7 +94,7 @@ def main():
 
     val_data_batch = DataLoader(val_dataset, batch_size=2*len(device_list), shuffle=False, drop_last=False, **kwargs)
     net = DeeplabV3Plus(lane_config)
-    if flag:
+    if torch.cuda.is_available():
         net = net.cuda(device=device_list[0])
         net = torch.nn.DataParallel(net, device_ids=device_list)
     optimizer = torch.optim.SGD(net.parameters(), lr=lane_config.BASE_LR,
@@ -104,7 +103,7 @@ def main():
     for epoch in range(lane_config.EPOCHS):
         # adjust_lr(optimizer, epoch)
         train_epoch(net, epoch, train_data_batch, optimizer, trainF, lane_config)
-        val_epoch(net, epoch, val_data_batch, testF, lane_config)
+        test(net, epoch, val_data_batch, testF, lane_config)
         if epoch % 10 == 0:
             torch.save(net, os.path.join(os.getcwd(), lane_config.SAVE_PATH, "laneNet{}.pth".format(epoch)))
     trainF.close()
