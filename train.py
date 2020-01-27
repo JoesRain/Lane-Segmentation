e8550dba238de6e73e0f0d1e6ceb2990ab3f8fde
yanzx <505554859@qq.com>
2020-01-27 20:04:51 +0800

    batch_size

--- train.py
+++ train.py
@@ -38,11 +38,9 @@ def train_epoch(net, epoch, dataLoader, optimizer, trainF, config):
         image = grid(image)
         out = net(image)
         mask_loss = MySoftmaxCrossEntropyLoss(nbclasses=config.NUM_CLASSES)(out, mask)
-        focal_value = focal_loss(out, mask)
-        loss = mask_loss + focal_value
         # total_mask_loss += loss.item()
-        total_mask_loss += loss.item() / accumulation_steps
-        loss.backward()
+        total_mask_loss += mask_loss.item() / accumulation_steps
+        mask_loss.backward()
         if ((i + 1) % accumulation_steps) == 0:
             optimizer.step()  # 反向传播，更新网络参数
             optimizer.zero_grad()  # 清空梯度
@@ -64,10 +62,8 @@ def test(net, epoch, dataLoader, testF, config):
             image, mask = image.cuda(device=device_list[0]), mask.cuda(device=device_list[0])
         out = net(image)
         mask_loss = MySoftmaxCrossEntropyLoss(nbclasses=config.NUM_CLASSES)(out, mask)
-        focal_value = focal_loss(out, mask)
-        loss = mask_loss + focal_value
         # total_mask_loss += mask_loss.item()
-        total_mask_loss += loss.detach().item()
+        total_mask_loss += mask_loss.detach().item()
     pred = torch.argmax(F.softmax(out, dim=1), dim=1)
     result = compute_iou(pred, mask, result)
     dataprocess.set_description_str("epoch:{}".format(epoch))
@@ -107,11 +103,11 @@ def main():
     train_dataset = LaneDataset("train.csv", transform=transforms.Compose([ImageAug(), DeformAug(),
                                                                            ScaleAug(), ToTensor()]))
 
-    train_data_batch = DataLoader(train_dataset, batch_size=2 * len(device_list), shuffle=True, drop_last=True,
+    train_data_batch = DataLoader(train_dataset, batch_size=4 * len(device_list), shuffle=True, drop_last=True,
                                   **kwargs)
     val_dataset = LaneDataset("val.csv", transform=transforms.Compose([ToTensor()]))
 
-    val_data_batch = DataLoader(val_dataset, batch_size=1 * len(device_list), shuffle=False, drop_last=False, **kwargs)
+    val_data_batch = DataLoader(val_dataset, batch_size=2 * len(device_list), shuffle=False, drop_last=False, **kwargs)
     net = DeeplabV3Plus(lane_config)
     # net = UNet(n_classes=8)
     if torch.cuda.is_available():
